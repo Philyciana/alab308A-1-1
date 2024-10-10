02ec3aed79ab98503874fbc5dc22419e5fd0d887
Philyciana <179555132+Philyciana@users.noreply.github.com>
2024-10-10 11:19:17 -0400

    flatten a array

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-let a = 4;
-function recursivefunc(i){
-    try {
-        a++;
-        setTimeout(recursivefunc, 1000); 
+// let a = 4;
+//function recursivefunc(i){
+  //  try {
+    //    a++;
+      //  console.log(a);
+        //setTimeout(() => recursivefunc(i), 1000); 
+    //}
+    //catch(error){
+      //  console.error("Error", error, a);
+    //}
+
+//}
+//recursivefunc(1); 
+
+//step2
+
+function flattenarray (array){
+    let result = [];
+    for (const item of array) {
+        if (Array.isArray(item)) {
+            result = result.concat(flattenarray(item));
+        } else {
+            result.push(item);
+        }
     }
-    catch(error){
-        console.error("Error", error), a;
-    }
+    return result;
 
 }
+const nestedarray = [1, 2, [3, 4, [5, 6]]];
+console.log(flattenarray(nestedarray));
